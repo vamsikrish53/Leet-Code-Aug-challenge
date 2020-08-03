f09863cf2d73d6703b74d2e341a4fa37f5452205
Krish <vamsikrish.49@gmail.com>
2020-08-03 18:43:43 +0530

    Day 3 solution

---
+++ Day 3/Solution.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] c = s.toCharArray();
+        for (int i = 0, j = c.length - 1; i < j; ) {
+            if (!Character.isLetterOrDigit(c[i])) i++;
+            else if (!Character.isLetterOrDigit(c[j])) j--;
+            else if (Character.toLowerCase(c[i++]) != Character.toLowerCase(c[j--])) 
+                return false;
+        }
+        return true;
+    }
+}
