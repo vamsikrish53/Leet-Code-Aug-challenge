0945f49b6280f6dec1bc9041855f720564df8185
Krish <vamsikrish.49@gmail.com>
2020-08-21 21:51:59 +0530

    Day 21 solution

---
+++ Day_ 21/Solution.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/sort-array-by-parity/
+
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int l = 0, r = A.length-1;
+        while(l < r){
+            while(l < r && A[l] % 2 == 0) l++;
+            while(l < r && A[r] % 2 == 1) r--;
+            int tmp = A[l];
+            A[l] = A[r];
+            A[r] = tmp;
+        }
+        return A;
+    }
+}
